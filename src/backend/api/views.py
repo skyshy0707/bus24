1e48fc2c814c0cc800f28570829b58c00feae373
Alexei Mironov <alexeidotpy@gmail.com>
2026-09-06 20:13:06 +0600

    Fix uncovered_leads endpoint to retrieve leads with respect atp leads only

--- src/backend/api/views.py
+++ src/backend/api/views.py
@@ -117,9 +117,10 @@ class UncoveredLeads(generics.ListAPIView):
     serializer_class = serializers.LeadSerializer
 
     def get_queryset(self):
+        atp_id = permissions.get_profile_id(self.request)
         return models.Lead.objects.annotate(
             total_units=Count('unit')
-        ).filter(units_per_route__gt=F('total_units'))
+        ).filter(Q(units_per_route__gt=F('total_units')) & Q(lead__atp__id=atp_id))
 
 
 class ProfileLeadsInfo(generics.ListAPIView):
